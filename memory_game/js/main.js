1a8ede6c3f798f816d4ccde04d6dd0671c86eef4
Eric Grieshaber <ericgrieshaber@Erics-MBP.fios-router.home>
2018-10-01 15:50:22 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,30 @@
 var cards = ["Queen", "Queen", "King", "King"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped Queen");
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped King");
+var checkForMatch = function() {
+	
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("Sorry, try again.");
+	}
+};
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-}	else {
-	alert("Sorry, try again.");
-}
+var flipCard = function(cardId) {
+	
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}	else {
+		alert("Sorry, try again.");
+	}	
+	checkForMatch();
+
+};
+
+flipCard(0);
+flipCard(2);
+
 
